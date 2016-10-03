841eb73cdbd1f77a227aa30f3eddb7a4ea9b55d7
pankaj <tiwari.pankaj@gmail.com>
2016-10-03 23:41:32 +0530

    hsqldb changes

--- src/main/java/com/test/domain/Userdetails.java
+++ src/main/java/com/test/domain/Userdetails.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "userdetails", schema = "assign")
+@Table(name = "userdetails")
 @NamedQueries(
         @NamedQuery(name = "userDetails.findByUserName", query = "SELECT U FROM Userdetails U WHERE U.userId=:userName")
 
